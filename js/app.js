d5f48fbc272994dec6105865dd2fcd9977305991
Michael <Michaelquiban@Outlook.com>
2017-01-11 23:18:10 -0800

    Removed unwanted comments

--- js/app.js
+++ js/app.js
@@ -26,9 +26,8 @@ salty = new Question("Do ye like it with a salty tang?");
 bitter = new Question("Are ye a lubber who likes it bitter?");
 sweet = new Question("Would ye like a bit of sweetness with yer poison?");
 fruity = new Question("Are ye one for a fruity finish?");
-//Strong, and Strong question should appear in the console.
+//Strong question should appear in the console.
 console.log(strong.type);
-console.log(strong.question);
 
 // Generate ingredients for the pantry
 var strongIngredient = new Ingredient("Strong", ["Glug of rum", "slug of whisky", "splash of gin"]);
@@ -36,7 +35,7 @@ saltyIngredient = new Ingredient("Salty", ["Olive on a stick", "salt-dusted rim"
 bitterIngredient = new Ingredient("Bitter", ["Shake of bitters", "splash of tonic", "twist of lemon peel"]);
 sweetIngredient = new Ingredient("Sweet", ["Sugar cube", "spoonful of honey", "splash of cola"]);
 fruityIngredient = new Ingredient("Fruity", ["Slice of orange", "dash of cassis", "cherry on top"]);
-//Strong ingredient description & Type should appear.
+//Strong ingredient description & Type should appear in the console.
 console.log(strongIngredient.type);
 console.log(strongIngredient.description);
 
