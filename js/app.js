e7753f66c8b2e80bbe1434097c6df1f261e2a741
Michael <Michaelquiban@Outlook.com>
2017-01-11 23:15:11 -0800

    Questions object should only contain questions, not type. Ingredients contain type and description of that ingredient.

--- js/app.js
+++ js/app.js
@@ -11,31 +11,33 @@ $( document ).ready(function() {
   this.question = question;
   };
 
-  var Ingredient = function(description) {
+  var Ingredient = function(type, description) {
     this.description = description;
   };
 
-  var Pantry = function(type, ingredient) {
+  var Pantry = function() {
     this.ingredient = {};
   };
 
 //Generate questions for the bartender using the Question constructor
-var strong = new Question("Strong", "Do ye like yer drinks strong?");
-salty = new Question("Salty", "Do ye like it with a salty tang?");
-bitter = new Question("Bitter", "Are ye a lubber who likes it bitter?");
-sweet = new Question("Sweet", "Would ye like a bit of sweetness with yer poison?");
-fruity = new Question("Fruity" ,"Are ye one for a fruity finish?");
+var strong = new Question("Do ye like yer drinks strong?");
+salty = new Question("Do ye like it with a salty tang?");
+bitter = new Question("Are ye a lubber who likes it bitter?");
+sweet = new Question("Would ye like a bit of sweetness with yer poison?");
+fruity = new Question("Are ye one for a fruity finish?");
 //Strong, and Strong question should appear in the console.
 console.log(strong.type);
 console.log(strong.question);
 
 // Generate ingredients for the pantry
-var strongIngredient = new Ingredient("Strong",["Glug of rum", "slug of whisky", "splash of gin"]);
-var saltyIngredient = new Ingredient("Salty",["Olive on a stick", "salt-dusted rim", "rasher of bacon"]);
-var bitterIngredient = new Ingredient("Bitter",["Shake of bitters", "splash of tonic", "twist of lemon peel"]);
-var sweetIngredient = new Ingredient("Sweet",["Sugar cube", "spoonful of honey", "splash of cola"]);
-var fruityIngredient = new Ingredient("Fruity",["Slice of orange", "dash of cassis", "cherry on top"]);
-//Strong ingredient, and description should appear.
+var strongIngredient = new Ingredient("Strong" ["Glug of rum", "slug of whisky", "splash of gin"]);
+saltyIngredient = new Ingredient("Salty" ["Olive on a stick", "salt-dusted rim", "rasher of bacon"]);
+bitterIngredient = new Ingredient("Bitter" ["Shake of bitters", "splash of tonic", "twist of lemon peel"]);
+sweetIngredient = new Ingredient("Sweet" ["Sugar cube", "spoonful of honey", "splash of cola"]);
+fruityIngredient = new Ingredient("Fruity" ["Slice of orange", "dash of cassis", "cherry on top"]);
+//Strong ingredient description & Type should appear.
+console.log(strongIngredient.type);
+console.log(strongIngredient.description);
 
 
 
